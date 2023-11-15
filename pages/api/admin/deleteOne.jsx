bcbcd90022385cf19107c83f9a9a973700e3fa53
zinkkii <zinkiiiii@gmail.com>
2023-11-15 17:33:34 +0900

    update

--- pages/api/admin/deleteOne.jsx
+++ pages/api/admin/deleteOne.jsx
@@ -2,7 +2,7 @@ import { executeQuery } from "../../../src/app/db/db";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    var sql = "DELETE FROM timeattack WHERE num = ?";
+    var sql = "DELETE FROM TimeAttack WHERE num = ?";
     try {
       let result = await executeQuery(sql, [req.body.num]);
       res.status(200).json(result);
--- pages/api/client/select.jsx
+++ pages/api/client/select.jsx
@@ -3,7 +3,7 @@ import { executeQuery } from "../../../src/app/db/db";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     var sql =
-      "SELECT * FROM timeattack ORDER BY laptime1 ASC, laptime2 ASC, laptime3 ASC";
+      "SELECT * FROM TimeAttack ORDER BY laptime1 ASC, laptime2 ASC, laptime3 ASC";
     try {
       let result = await executeQuery(sql, []);
       res.status(200).json(result);
